fa6d0f248dd5a2089b5ee3706b1e11df36630b92
coi <coikarp175@gmail.com>
2023-01-02 20:44:18 +0600

    Initial commit #2

---
+++ src/main/java/com/coi/contactcenterapp/ContactCenterAppApplication.java
@@ -0,0 +1,13 @@
+package com.coi.contactcenterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactCenterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactCenterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coi/contactcenterapp/ContactCenterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coi.contactcenterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactCenterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
